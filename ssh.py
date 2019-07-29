3571b3a93d946eb2eccdeac74977a6f31a4e7432
gunzrun <d.almuhametov@gmail.com>
2019-07-29 08:27:29 +0300

    done some templates and ssh connections at home

--- ssh.py
+++ ssh.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python3
 import netmiko
+import textfsm
 
 from helper import read_yaml, form_connection_params_from_yaml
 
-SITE_NAME = "SJ-HQ"
+#SITE_NAME = "SJ-HQ"
 
 COMMANDS_LIST = [
     "show clock", "show version", "show inventory", "show ip interface brief"
 ]
 
-
 def collect_outputs(devices, commands):
     """
-    Collects commands from the dictionary of devices
+    Collects commands from the list of devices
     Args:
-        devices (dict): dictionary, where key is the hostname, value is
-            netmiko connection dictionary
+        devices (list): list of netmiko connection dictionaries
         commands (list): list of commands to be executed on every device
     Returns:
         dict: key is the hostname, value is string with all outputs
     """
     for device in devices:
-        hostname = device.pop("hostname")
+#        hostname = device.pop("hostname")
         connection = netmiko.ConnectHandler(**device)
-        device_result = ["{0} {1} {0}".format("=" * 20, hostname)]
+        device_result = ["{0} {1} {0}".format("=" * 20, device)]
 
         for command in commands:
             command_result = connection.send_command(command)
             device_result.append("{0} {1} {0}".format("=" * 20, command))
             device_result.append(command_result)
+
+
+        with open('/Users/den/PycharmProjects/netbox_utils/out_templates/cisco_ios_show_ip_int_brief.template') as template:
+            fsm = textfsm.TextFSM(template)
+            result_out = fsm.ParseText(result)
 
         device_result_string = "\n\n".join(device_result)
         connection.disconnect()
