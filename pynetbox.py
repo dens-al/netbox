c5455a9dfb0f7f60b9636eb6a6c56cb4f13cd275
Denis Almukhametov <d.almuhametov@gmail.com>
2019-07-31 17:56:45 +0300

    found out about pynetbox

--- pynetbox.py
+++ pynetbox.py
@@ -1,10 +1,43 @@
 import pynetbox
+import re
 
 NETBOX_URL = "http://10.10.1.100:8085"
 NETBOX_API_TOKEN = '454a75712e782741166ed11fe711e3d5f9476e05'
-
+NETBOX_ROLES = {
+    'br': 'Router',
+    'ir': 'Router',
+    'vg': 'Voice Gateway',
+    'csp': 'Crypto',
+    'fw': 'Firewall',
+    'csw': 'Core Switch',
+    'asw': 'Access Switch',
+    'dsw': 'Distribution Switch',
+    'bsw': 'Border Switch'
+}
 nb = pynetbox.api(url=NETBOX_URL, token=NETBOX_API_TOKEN)
 
-def create_site(name):
 
-def create_device(name, model_id, manufacture_id, role_id, site_id, serial=None):
+def create_site(name):
+    "Check if site doesn't exist add new site with same slug"
+    sites = [str(site) for site in nb.dcim.sites.all()]
+    if name not in sites:
+        print('site {site} not exists. Creating site'.format(site=name))
+        nb.dcim.sites.create(name=name, slug=name)
+        print('...done')
+    else:
+        print('site {site} has found'.format(site=name))
+
+
+def main():
+    """
+    For parse site and device role from hostname use regex.
+    For example: hostname is msk02-gpn-asw02. For site use first symbols, before "-"
+    for role use symbols after last "-" (msk02, asw02)
+    """
+    regex = '(?P<site>\S+?)-(?P<role>\S+$)'
+    hostname = 'msk02-br01'
+    match = re.search(regex, hostname)
+    device_site = match.group('site')
+    device_role = match.group('role')
+
+    create_site(device_site)
